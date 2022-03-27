6ceff48a5f309d466be8666ecd81bb668ece4b02
first version <>
2022-03-27 18:20:40 +0100

    Scripts for plotting Stromgren sphere with hydrogen and helium

--- examples/RadiativeTransferTests/StromgrenSphere_3D/plotStromgren3DMFHHeCheck.py
+++ examples/RadiativeTransferTests/StromgrenSphere_3D/plotStromgren3DMFHHeCheck.py
@@ -9,6 +9,7 @@
 import swiftsimio
 from matplotlib import pyplot as plt
 import matplotlib as mpl
+import matplotlib.lines as mlines
 import numpy as np
 import copy
 import unyt
@@ -43,14 +44,12 @@ params = {
 'lines.linewidth' : 2.,
 }
 mpl.rcParams.update(params)
-mpl.rc('font',**{'family':'sans-serif','sans-serif':['Times']})
 
 scatterplot_kwargs = {
-    "alpha": 0.6,
+    "alpha": 0.1,
     "s": 4,
     "marker": ".",
-    "linewidth": 0.0,
-    "facecolor": "blue",
+    "linewidth": 0.0
 }
 
 # Read in cmdline arg: Are we plotting only one snapshot, or all?
@@ -66,18 +65,56 @@ snapshot_base = "output"
 
 def get_TT1Dsolution():
     TT1D_runit = 5.4 * unyt.unyt_array(1.0,'kpc') #kpc
-    xtt1dlist=np.array([])
-    rtt1dlist=np.array([])
-    with open('data/xTT1D_Stromgren100Myr.csv') as csvfile:
+    xHItt1dlist=np.array([])
+    rHItt1dlist=np.array([])
+    with open('data/xHITT1D_Stromgren100Myr_HHe.csv') as csvfile:
         spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
         for row in spamreader:
-            rtt1dlist=np.append(rtt1dlist,float(row[0]))
-            xtt1dlist=np.append(xtt1dlist,10**float(row[1]))
-    rtt1dlist *= TT1D_runit
-
+            rHItt1dlist=np.append(rHItt1dlist,float(row[0]))
+            xHItt1dlist=np.append(xHItt1dlist,10**float(row[1]))
+    rHItt1dlist *= TT1D_runit
+    
+    
+    xHIItt1dlist=np.array([])
+    rHIItt1dlist=np.array([])
+    with open('data/xHIITT1D_Stromgren100Myr_HHe.csv') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+        for row in spamreader:
+            rHIItt1dlist=np.append(rHIItt1dlist,float(row[0]))
+            xHIItt1dlist=np.append(xHIItt1dlist,10**float(row[1]))
+    rHIItt1dlist *= TT1D_runit
+    
+    xHeItt1dlist=np.array([])
+    rHeItt1dlist=np.array([])
+    with open('data/xHeITT1D_Stromgren100Myr_HHe.csv') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+        for row in spamreader:
+            rHeItt1dlist=np.append(rHeItt1dlist,float(row[0]))
+            xHeItt1dlist=np.append(xHeItt1dlist,10**float(row[1]))
+    rHeItt1dlist *= TT1D_runit
+    
+    xHeIItt1dlist=np.array([])
+    rHeIItt1dlist=np.array([])
+    with open('data/xHeIITT1D_Stromgren100Myr_HHe.csv') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+        for row in spamreader:
+            rHeIItt1dlist=np.append(rHeIItt1dlist,float(row[0]))
+            xHeIItt1dlist=np.append(xHeIItt1dlist,10**float(row[1]))
+    rHeIItt1dlist *= TT1D_runit    
+    
+    xHeIIItt1dlist=np.array([])
+    rHeIIItt1dlist=np.array([])
+    with open('data/xHeIIITT1D_Stromgren100Myr_HHe.csv') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+        for row in spamreader:
+            rHeIIItt1dlist=np.append(rHeIIItt1dlist,float(row[0]))
+            xHeIIItt1dlist=np.append(xHeIIItt1dlist,10**float(row[1]))
+    rHeIIItt1dlist *= TT1D_runit  
+    
+    
     Ttt1dlist=np.array([])
     rTtt1dlist=np.array([])
-    with open('data/TTT1D_Stromgren100Myr.csv') as csvfile:
+    with open('data/TTT1D_Stromgren100Myr_HHe.csv') as csvfile:
         spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
         for row in spamreader:
             rTtt1dlist=np.append(rTtt1dlist,float(row[0]))
@@ -85,8 +122,16 @@ def get_TT1Dsolution():
     rTtt1dlist *= TT1D_runit
     Ttt1dlist *= unyt.unyt_array(1.0, 'K')
     outdict = {}
-    outdict['rtt1dlist'] = rtt1dlist
-    outdict['xtt1dlist'] = xtt1dlist
+    outdict['rHItt1dlist'] = rHItt1dlist
+    outdict['xHItt1dlist'] = xHItt1dlist
+    outdict['rHIItt1dlist'] = rHIItt1dlist
+    outdict['xHIItt1dlist'] = xHIItt1dlist
+    outdict['rHeItt1dlist'] = rHeItt1dlist
+    outdict['xHeItt1dlist'] = xHeItt1dlist
+    outdict['rHeIItt1dlist'] = rHeIItt1dlist
+    outdict['xHeIItt1dlist'] = xHeIItt1dlist
+    outdict['rHeIIItt1dlist'] = rHeIIItt1dlist
+    outdict['xHeIIItt1dlist'] = xHeIIItt1dlist
     outdict['rTtt1dlist'] = rTtt1dlist
     outdict['Ttt1dlist'] = Ttt1dlist    
     return outdict
@@ -176,6 +221,33 @@ def get_imf(scheme,data):
     return imf
 
 
+def get_abundances(scheme,data):
+    """
+    Get the species abundance in n_X/n_H according to the scheme.
+    """
+    if scheme.startswith("GEAR M1closure"):
+        # atomic mass
+        mamu = {'e':0.0, 'HI':1.0, 'HII':1.0, 'HeI':4.0, 'HeII':4.0, 'HeIII':4.0}
+        sA = copy.deepcopy(data.gas.ion_mass_fractions)
+        mass_fraction_hydrogen = data.gas.ion_mass_fractions.HI + data.gas.ion_mass_fractions.HII
+        # abundance is in n_X/n_H unit. We convert mass fraction to abundance by dividing mass fraction of H
+        abundance = getattr(data.gas.ion_mass_fractions,'HI') / mass_fraction_hydrogen / mamu['HI']
+        setattr(sA,'HI', abundance)        
+        abundance = getattr(data.gas.ion_mass_fractions,'HII') / mass_fraction_hydrogen / mamu['HII']
+        setattr(sA,'HII', abundance)
+        abundance = getattr(data.gas.ion_mass_fractions,'HeI') / mass_fraction_hydrogen / mamu['HeI']
+        setattr(sA,'HeI', abundance)        
+        abundance = getattr(data.gas.ion_mass_fractions,'HeII') / mass_fraction_hydrogen / mamu['HeII']
+        setattr(sA,'HeII', abundance)
+        abundance = getattr(data.gas.ion_mass_fractions,'HeIII') / mass_fraction_hydrogen / mamu['HeIII']
+        setattr(sA,'HeIII', abundance)
+    elif scheme.startswith("SPH M1closure"):
+        sA = data.gas.rt_species_abundances
+    return sA
+
+
+
+
 def trim_paramstr(paramstr):
     # clean string up
     if paramstr.startswith("["):
@@ -206,35 +278,59 @@ def plot_compare(filename):
     r = np.sqrt(np.sum(dxp ** 2, axis=1))
 
     imf = get_imf(scheme,data)
-    xHI = imf.HI/(imf.HI+imf.HII)
 
     mu = mean_molecular_weight(imf.HI, imf.HII, imf.HeI, imf.HeII, imf.HeIII)
     data.gas.T = gas_temperature(data.gas.internal_energies, mu, gamma)
 
+    sA = get_abundances(scheme,data)
+    xHI = sA.HI
+    xHII = sA.HII
+    xHeI = sA.HeI
+    xHeII = sA.HeII
+    xHeIII = sA.HeIII
 
     outdict = get_TT1Dsolution()
 
     fig, ax = plt.subplots(1,2)
 
-    ax[0].scatter(r,xHI, **scatterplot_kwargs)
-    ax[0].plot(outdict['rtt1dlist'], outdict['xtt1dlist'], color='k',lw=2.0,label="TT1D")
-    ax[0].set_ylabel('Neutral Fraction')
+    ax[0].scatter(r,xHI, **scatterplot_kwargs,facecolor='k')
+    ax[0].scatter(r,xHII, **scatterplot_kwargs,facecolor='r')
+    ax[0].scatter(r,xHeI, **scatterplot_kwargs,facecolor='b')
+    ax[0].scatter(r,xHeII, **scatterplot_kwargs,facecolor='g')
+    ax[0].scatter(r,xHeIII, **scatterplot_kwargs,facecolor='y')
+    ax[0].plot(outdict['rHItt1dlist'], outdict['xHItt1dlist'], color='k',lw=2.0,ls='dashed',label="TT1D")
+    ax[0].plot(outdict['rHIItt1dlist'], outdict['xHIItt1dlist'], color='r',lw=2.0,ls='dashed')
+    ax[0].plot(outdict['rHeItt1dlist'], outdict['xHeItt1dlist'], color='b',lw=2.0,ls='dashed')
+    ax[0].plot(outdict['rHeIItt1dlist'], outdict['xHeIItt1dlist'], color='g',lw=2.0,ls='dashed')
+    ax[0].plot(outdict['rHeIIItt1dlist'], outdict['xHeIIItt1dlist'], color='y',lw=2.0,ls='dashed')
+    ax[0].set_ylabel('Abundances')
     xlabel_units_str = meta.boxsize.units.latex_representation()
     ax[0].set_xlabel("r [$" + xlabel_units_str + "$]") 
     ax[0].set_yscale('log')
-    ax[0].set_xlim([0,boxsize[0]/2.0])
+    ax[0].set_xlim([0,5.4*1.3])
+    ax[0].set_ylim([1e-5,1.1])
+    ax[0].legend(loc='best', fontsize=12)
 
     ax[1].scatter(r,data.gas.T, **scatterplot_kwargs)
-    ax[1].plot(outdict['rTtt1dlist'], outdict['Ttt1dlist'], color='k',lw=2.0,label="TT1D")
+    ax[1].plot(outdict['rTtt1dlist'], outdict['Ttt1dlist'], color='k',lw=2.0,label="TT1D", ls='dashed')
     ax[1].set_ylabel('T [K]')
     xlabel_units_str = meta.boxsize.units.latex_representation()
     ax[1].set_xlabel("r [$" + xlabel_units_str + "$]") 
     ax[1].set_yscale('log')
-    ax[1].set_xlim([0,boxsize[0]/2.0])
+    ax[1].set_xlim([0,5.4*1.3])
+    HI_line = mlines.Line2D([], [], lw=2, ls='dashed', label="HI", color='k')
+    HII_line = mlines.Line2D([], [], lw=2, ls='dashed', label="HII", color='r')
+    HeI_line = mlines.Line2D([], [], lw=2, ls='dashed', label="HeI", color='b')
+    HeII_line = mlines.Line2D([], [], lw=2, ls='dashed', label="HeII", color='g')
+    HeIII_line = mlines.Line2D([], [], lw=2, ls='dashed', label="HeIII", color='y')
+    first_legend = ax[1].legend(handles=[HI_line,HII_line,HeI_line,HeII_line,HeIII_line], loc='best', fontsize=12)
+    ax[1].add_artist(first_legend)
+
+
 
     plt.tight_layout()
     figname = filename[:-5]
-    figname += "-Stromgren3DMF.png"
+    figname += "-Stromgren3DMFHHe.png"
     plt.savefig(figname, dpi=200)
     plt.close()
 
